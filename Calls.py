52963f7c2d7732b4344751ae163a5fd3d5e35d83
Haim <haimor1123@gmail.com>
2021-11-09 20:55:09 +0200

    new class(Calls)

--- Calls.py
+++ Calls.py
@@ -1,14 +1,21 @@
 import csv
 
-class calls:
 
+class Calls:
 
-    def from_csv(csvFile):
-        with open(csvFile) as mycsv_file:
-            csv_reader = csv.reader(mycsv_file)
-            callsLine=list(csv_reader)
-        return callsLine
+    def from_csvTolist(csvFile):
+        with open(csvFile) as csv_file:
+            csv_reader = csv.reader(csv_file)
+            rows = list(csv_reader)
+        return rows
 
+    def allocate(src, dest):
+        if (src < dest):  # Up
+            return 0
+        else:
+            return 1
 
-
-
+    def write_tofile(rows):
+        with open('Ex1_Output/Ex1_Calls_case_1_b.csv', 'w', encoding='UTF8', newline='') as csv_file:
+            write = csv.writer(csv_file)
+            write.writerows(rows)
--- Ex1_main.py
+++ Ex1_main.py
@@ -2,24 +2,23 @@ import json
 import csv
 from Buildings import Buildings
 from Elevator import Elevator
+from Calls import Calls
 
-rows = []
-with open('Ex1_Calls/Calls_a.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    rows = list(csv_reader)
-    for x in range(len(rows)):
-        rows[x][5] = 0
-    print(rows)
-
-with open('Ex1_Output/Ex1_Calls_case_1_b.csv', 'w', encoding='UTF8', newline='') as csv_file:
-    write = csv.writer(csv_file)
-    write.writerows(rows)
+call_list = Calls.from_csvTolist('Ex1_Calls/Calls_b.csv')
+print(call_list)
 
 B1 = Buildings.from_json('Ex1_Buildings/B2.json')
 Elist = (B1['_elevators'])
 Elevator_list = []
 for x in range(len(Elist)):
     Elevator_list.append(Elevator(Elist, x))
-for x in range(len(Elevator_list)):
-    print(Elevator_list[x].printelev())
 print("-------------------------------------------------------")
+for ilan in range(len(call_list)):
+    x=Calls.allocate(call_list[ilan][2],call_list[ilan][3])
+    call_list[ilan][5] = x
+
+print(call_list)
+
+with open('Ex1_Output/Ex1_Calls_case_1_b.csv', 'w', encoding='UTF8', newline='') as csv_file:
+    write = csv.writer(csv_file)
+    write.writerows(call_list)
