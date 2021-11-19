99e51488cb61aa646c906499fb2191d93fee5f4a
Ilan Gold <goldlmilan@gmail.com>
2021-11-19 19:16:35 +0200

    final version

--- Ex1_main.py
+++ Ex1_main.py
@@ -1,5 +1,6 @@
 import json
 import csv
+import subprocess
 import sys
 
 from Buildings import Buildings
@@ -7,17 +8,9 @@ from Elevator import *
 from Calls import Calls
 from ElevatorAlgo import ElevatorAlgo
 
+algo1 = ElevatorAlgo(sys.argv[1], sys.argv[2], sys.argv[3])
+algo1.allocate()
+algo1.write_tofile(sys.argv[3])
 
-
-
-#
-if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        algo1 = ElevatorAlgo(sys.argv[1], sys.argv[2], sys.argv[3])
-        algo1.allocate()
-        algo1.write_tofile(sys.argv[3])
-    else:
-        algo1 = ElevatorAlgo('Ex1_Buildings/B4.json', 'Ex1_Calls/Calls_c.csv', 'out.csv')
-        algo1.allocate()
-        algo1.write_tofile('Ex1_Output/out.csv')
-
+subprocess.Popen(["powershell.exe",
+                  "java -jar Ex1_checker_V1.2_obf.jar 315804583,319147419 " + sys.argv[1] + " " + sys.argv[3] + " out.log"])
