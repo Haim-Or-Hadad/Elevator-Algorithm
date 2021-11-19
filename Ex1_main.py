4ddcfa53add24eac6c8cc5282c55cdba75ec4de4
Ilan Gold <goldlmilan@gmail.com>
2021-11-19 17:22:46 +0200

    new class(Calls)

--- Ex1_main.py
+++ Ex1_main.py
@@ -1,10 +1,23 @@
 import json
 import csv
+import sys
+
 from Buildings import Buildings
 from Elevator import *
 from Calls import Calls
 from ElevatorAlgo import ElevatorAlgo
 
-algo1 = ElevatorAlgo('Ex1_Buildings/B4.json', 'Ex1_Calls/Calls_c.csv', 'out.csv')
-algo1.allocate()
-algo1.write_tofile('Ex1_Output\Ex1_Calls_case_1_b.csv')
+
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        algo1 = ElevatorAlgo(sys.argv[1], sys.argv[2], sys.argv[3])
+        algo1.allocate()
+        algo1.write_tofile(sys.argv[3])
+    else:
+        algo1 = ElevatorAlgo('Ex1_Buildings/B4.json', 'Ex1_Calls/Calls_c.csv', 'out.csv')
+        algo1.allocate()
+        algo1.write_tofile('Ex1_Output/out.csv')
+
